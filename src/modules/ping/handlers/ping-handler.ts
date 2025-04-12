330aab96a3360444bd919d47b0a0ff33e4f11b36
Jack Maders <jackwmaders@gmail.com>
2025-04-12 23:49:10 +0100

    feat: :sparkles: add placeholder ping command

---
+++ src/modules/ping/handlers/ping-handler.ts
@@ -0,0 +1,22 @@
+import type { CacheType, ChatInputCommandInteraction } from "discord.js";
+import { MessageFlags } from "discord.js";
+
+export default async function handlePing(
+	interaction: ChatInputCommandInteraction<CacheType>,
+): Promise<void> {
+	// Temporary hard-coded text
+	// Will be replaced a translation function when i18n is implemented.
+	await interaction.reply({
+		content: "Pinging...",
+		flags: [MessageFlags.Ephemeral],
+	});
+
+	const latency = Date.now() - interaction.createdTimestamp;
+	const websocketHeartbeat = interaction.client.ws.ping;
+
+	// Temporary hard-coded text
+	// Will be replaced a translation function when i18n is implemented.
+	await interaction.editReply(
+		`Pong! 🏓\nRound trip latency: ${latency}ms\nWebSocket heartbeat: ${websocketHeartbeat}ms`,
+	);
+}
