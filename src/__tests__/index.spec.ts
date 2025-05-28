6717a9566e3dba307949d40f5aff5b8859f33431
Jack Maders <jackwmaders@gmail.com>
2025-05-28 15:45:19 +0100

    test: :test_tube: improve unit tests

--- src/__tests__/index.spec.ts
+++ src/__tests__/index.spec.ts
@@ -1,5 +1,5 @@
 import { type ClientUser, Events } from "discord.js";
-import { type Mock, afterEach, describe, expect, it, vi } from "vitest";
+import { afterAll, afterEach, describe, expect, it, vi } from "vitest";
 import discordClient from "../core/clients/discord.client";
 import logMessages from "../core/constants/log-messages";
 import commandsService from "../core/services/commands.service";
@@ -10,36 +10,38 @@ import exitProcess from "../core/utils/exit-process";
 
 describe("index.ts", () => {
 	afterEach(() => {
-		vi.unstubAllEnvs();
 		vi.resetModules();
 		vi.resetAllMocks();
 	});
 
-	it("should initialise the LoggerService", async () => {
+	afterAll(() => {
+		vi.unstubAllEnvs();
+		vi.unstubAllGlobals();
+	});
+
+	it("should initialise all core services", async () => {
+		// Act
 		await import("../index");
+
+		// Assert
 		expect(loggerService.initialise).toHaveBeenCalled();
-	});
-
-	it("should initialise the SchedulerService", async () => {
-		await import("../index");
 		expect(schedulerService.initialise).toHaveBeenCalled();
-	});
-
-	it("should initialise the TranslationService", async () => {
-		await import("../index");
 		expect(translationService.initialise).toHaveBeenCalled();
-	});
-
-	it("should initialise the CommandsService", async () => {
-		await import("../index");
 		expect(commandsService.initialise).toHaveBeenCalled();
 	});
 
-	it("should assign the Error discord.js event fn", async () => {
+	it("should assign the Error discord.js event listener", async () => {
+		// Arrange
 		const error = new Error("Test error");
-		(discordClient.on as Mock).mockImplementation((_, fn) => fn(error));
+		vi.mocked(discordClient).on.mockImplementation((_, fn) => {
+			fn(error);
+			return discordClient;
+		});
 
+		// Act
 		await import("../index");
+
+		// Assert
 		expect(discordClient.on).toHaveBeenCalledWith(
 			Events.Error,
 			expect.any(Function),
@@ -47,11 +49,18 @@ describe("index.ts", () => {
 		expect(loggerService.error).toHaveBeenCalledWith(error);
 	});
 
-	it("should assign the Warn discord.js event fn", async () => {
+	it("should assign the Warn discord.js event listener", async () => {
+		// Arrange
 		const message = "Test warning";
-		(discordClient.on as Mock).mockImplementation((_, fn) => fn(message));
+		vi.mocked(discordClient).on.mockImplementation((_, fn) => {
+			fn(message);
+			return discordClient;
+		});
 
+		// Act
 		await import("../index");
+
+		// Assert
 		expect(discordClient.on).toHaveBeenCalledWith(
 			Events.Warn,
 			expect.any(Function),
@@ -59,12 +68,20 @@ describe("index.ts", () => {
 		expect(loggerService.warn).toHaveBeenCalledWith(message);
 	});
 
-	it("should assign the InteractionCreate discord.js event fn", async () => {
+	it("should assign the InteractionCreate discord.js event listener", async () => {
+		// Arrange
 		const interaction = {
 			message: "Test interaction",
 		};
-		(discordClient.on as Mock).mockImplementation((_, fn) => fn(interaction));
+		vi.mocked(discordClient).on.mockImplementation((_, fn) => {
+			fn(interaction);
+			return discordClient;
+		});
+
+		// Act
 		await import("../index");
+
+		// Assert
 		expect(discordClient.on).toHaveBeenCalledWith(
 			Events.InteractionCreate,
 			expect.any(Function),
@@ -72,9 +89,17 @@ describe("index.ts", () => {
 		expect(commandsService.handleInteraction).toHaveBeenCalledWith(interaction);
 	});
 
-	it("should assign the ClientReady discord.js event fn", async () => {
-		(discordClient.once as Mock).mockImplementation((_, fn) => fn());
+	it("should assign the ClientReady discord.js event listener", async () => {
+		// Arrange
+		vi.mocked(discordClient).once.mockImplementation((_, fn) => {
+			fn();
+			return discordClient;
+		});
+
+		// Act
 		await import("../index");
+
+		// Assert
 		expect(discordClient.once).toHaveBeenCalledWith(
 			Events.ClientReady,
 			expect.any(Function),
@@ -83,11 +108,15 @@ describe("index.ts", () => {
 	});
 
 	it("should login to the discord client", async () => {
-		discordClient.user = { tag: "TestUser#1234" } as ClientUser;
+		// Arrange
+		vi.stubEnv("DISCORD_BOT_TOKEN", "DISCORD_BOT_TOKEN");
+		vi.mocked(discordClient).user = { tag: "TestUser#1234" } as ClientUser;
+
+		// Act
 		await import("../index");
-		expect(discordClient.login).toHaveBeenCalledWith(
-			process.env.DISCORD_BOT_TOKEN,
-		);
+
+		// Assert
+		expect(discordClient.login).toHaveBeenCalledWith("DISCORD_BOT_TOKEN");
 		expect(loggerService.info).toHaveBeenCalledWith(
 			logMessages.INFO_BOT_LOGIN,
 			discordClient.user?.tag,
@@ -95,24 +124,29 @@ describe("index.ts", () => {
 	});
 
 	it("should log an error and exit on failure", async () => {
+		// Arrange
 		const error = new Error("Login failed");
-		(discordClient.login as Mock).mockRejectedValueOnce(error);
-		vi.spyOn(console, "error").mockImplementation(() => {});
+		vi.mocked(discordClient).login.mockRejectedValueOnce(error);
+		vi.spyOn(console, "error").mockImplementation(vi.fn());
 
+		// Act
 		await import("../index");
 
+		// Assert
 		expect(console.error).toHaveBeenCalledWith(error);
 		expect(exitProcess).toHaveBeenCalledWith(1);
 	});
 
 	it("should setup even handlers for SIGINT and SIGTERM", async () => {
-		vi.spyOn(process, "on").mockImplementation((_, fn) => fn() as never);
+		// Arrange
+		vi.spyOn(process, "on").mockImplementation(vi.fn());
 
+		// Act
 		await import("../index");
 
+		// Assert
 		expect(process.on).toHaveBeenCalledWith("SIGINT", expect.any(Function));
 		expect(process.on).toHaveBeenCalledWith("SIGTERM", expect.any(Function));
-		expect(exitProcess).toHaveBeenCalledTimes(2);
 	});
 });
 
